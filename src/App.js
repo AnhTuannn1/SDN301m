6025ce95a0388c26490c7da7bf73ce3d082cf3bb
anhtuan333 <jackienguyennn01@gmail.com>
2023-09-08 17:19:50 +0700

    test

--- src/App.js
+++ src/App.js
@@ -21,5 +21,29 @@ function App() {
     </div>
   );
 }
+function cal() {
+  var rect = {
+    perimeter: (x, y) => (2*(x+y)),
+    area: (x, y) => (x*y)
+};
 
-export default App;
+function solveRect(l,b) {
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+
+    if (l <= 0 || b <= 0) {
+        console.log("Rectangle dimensions should be greater than zero:  l = "
+               + l + ",  and b = " + b);
+    }
+    else {
+        console.log("The area of the rectangle is " + rect.area(l,b));
+        console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
+
+} 
+export default cal;
